61e1876fc928a3c5edd65775a879478c2b0b8fa3
Niño Bancoro <bancoronino5@gmail.com>
2026-09-01 14:08:03 +0000

    Harden public viewing repo by removing portal code

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,12 @@
 import { RouterProvider } from "react-router";
 import { router } from "./routes";
 import { Toaster } from "sonner";
-import { AuthProvider } from "../contexts/AuthContext";
 
 export default function App() {
   return (
-    <AuthProvider>
+    <>
       <RouterProvider router={router} />
       <Toaster position="top-right" richColors />
-    </AuthProvider>
+    </>
   );
 }
